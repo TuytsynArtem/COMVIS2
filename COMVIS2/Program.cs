b0924bf536199a2a88fb7ff794e28cb0f67af3e9
User <User@DESKTOP-L9LEDCK>
2019-05-13 17:14:19 +0300

    aAAAAAAAAAAAAAAAAAAAA

--- COMVIS2/Program.cs
+++ COMVIS2/Program.cs
@@ -1,21 +1,158 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace COMVIS2
+namespace CSHttpClientSample
 {
-    class Program
+    public class Caption
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        public string text { get; set; }
+        public double confidence { get; set; }
+    }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    public class Description
+    {
+        public List<string> tags { get; set; }
+        public List<Caption> Captions { get; set; }
+    }
+
+    public class Metadata
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public string format { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Description description { get; set; }
+        public string requestId { get; set; }
+        public Metadata metadata { get; set; }
+    }
+    static class Program
+    {
+        // Replace <Subscription Key> with your valid subscription key.
+        const string subscriptionKey = "18b549152ccb4ba5a5fe0bac34f6af35";
+
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        const string uriBase =
+            "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Analyze an image:");
+            Console.Write(
+                "Enter the path to the image you wish to analyze: ");
+            string imageFilePath = Console.ReadLine();
+
+            if (File.Exists(imageFilePath))
+            {
+                // Call the REST API method.
+                Console.WriteLine("\nWait a moment for the results to appear.\n");
+                MakeAnalysisRequest(imageFilePath).Wait();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using
+        /// the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to analyze.</param>
+        static async Task MakeAnalysisRequest(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                // The Analyze Image method returns information about the following
+                // visual features:
+                // Categories:  categorizes image content according to a
+                //              taxonomy defined in documentation.
+                // Description: describes the image content with a complete
+                //              sentence in supported languages.
+                // Color:       determines the accent color, dominant color, 
+                //              and whether an image is black & white.
+                string requestParameters =
+                    "visualFeatures=Description";
+
+                // Assemble the URI for the REST API method.
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Read the contents of the specified local image
+                // into a byte array.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                // Add the byte array as an octet stream to the request body.
+                string precontentString = "";
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses the "application/octet-stream" content type.
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Asynchronously call the REST API method.
+                    response = await client.PostAsync(uri, content);
+                     
+                }
+               
+                precontentString = await response.Content.ReadAsStringAsync();
+                // Asynchronously get the JSON response.
+                var rateInfo = JsonConvert.DeserializeObject<RootObject>(precontentString);
+                 var score = rateInfo.description.Captions;
+
+
+                
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n\n{0}\n", score);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+        /*JToken.Parse(precontentString).ToString()*/
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
         }
     }
 }
